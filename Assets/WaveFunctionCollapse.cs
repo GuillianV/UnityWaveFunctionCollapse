5998e77384b7bbe226c517f9d667b5887f4db805
GuillianV <guillianvibert90@gmail.com>
2022-08-11 22:27:51 +0200

    add new chunks and options

--- Assets/WaveFunctionCollapse.cs
+++ Assets/WaveFunctionCollapse.cs
@@ -7,20 +7,35 @@ using UnityEditor;
 using UnityEngine;
 
 
-public static class Options
+public class Options
 {
-    public const int BLANK = 0;
-    public const int LEFT = 1;
-    public const int TOP = 2;
-    public const int RIGHT = 3;
-    public const int DOWN = 4;
-   
-    //Need implement data structure and rules
+    public Dictionary<int, string> options = new Dictionary<int, string>();
 
-    public static List<int> GetAllOptions()
+    public Options(List<Chunck> chuncks)
     {
-        return new List<int>() { BLANK,LEFT, TOP, RIGHT, DOWN };
+     
+        chuncks?.ForEach(chunck =>
+        {
+            string optionValue = string.Format("{0}-{1}-{2}-{3}", chunck.Edges.left.patern, chunck.Edges.top.patern,
+                chunck.Edges.right.patern, chunck.Edges.down.patern);
+
+            if (!options.ContainsValue(optionValue))
+            {
+                options.Add(options.Count+1,optionValue);
+            }
+        });
+        
+        
     }
+
+    public int[] GetAllOptions()
+    {
+        return options.Keys.ToArray();
+    }
+    
+    
+    
+
 }
 
 [System.Serializable]
@@ -42,7 +57,7 @@ public struct SquareEdge
 public class Chunck
 {
   
-    public GameObject gridToInstanciate;
+    public GameObject gridToInstanciate ;
 
     [Header("Rules")]
     public SquareEdge Edges;
@@ -52,45 +67,22 @@ public class Chunck
     public bool down;
 
     [CanBeNull]
-    public Chunck GetChunck(int index)
+    public Chunck GetChunck(int index, Options options)
     {
-        Chunck myChunck = null;
-        switch (index)
-        {
-            case Options.BLANK :
-                break;
-            
-            case Options.LEFT:
-                if (left)
-                {
-                    myChunck =this;
-                } 
-                break;
-            
-            case Options.TOP:
-                if (top)
-                {
-                    myChunck =this;
-                } 
-                break;
-            
-            case Options.RIGHT:
-                if (right)
-                {
-                    myChunck =this;
-                } 
-                break;
-            
-            case Options.DOWN:
-                if (down)
-                {
-                    myChunck =this;
-                } 
-                break;
-                
-        }
+       KeyValuePair<int,string> valuePair = options.options.FirstOrDefault(kpo => {return kpo.Key == index;});
+       string[] edges = valuePair.Value.Split('-');
 
-        return myChunck;
+       Chunck chunckFound = null;
+       if (edges.Length == 4)
+       {
+           if (edges[0] == Edges.left.patern && edges[1] == Edges.top.patern && edges[2] == Edges.right.patern  && edges[3] == Edges.down.patern  )
+           {
+               chunckFound = this;
+           }
+           
+       }
+       return chunckFound;
+
     }
     
 }
@@ -98,7 +90,13 @@ public class Chunck
 public class GridChunck
 {
     public bool collapsed = false;
-    public List<int> optionsAvailable = Options.GetAllOptions();
+    public int[] optionsAvailable;
+
+    public GridChunck(int[] _optionsAvailable)
+    {
+        optionsAvailable = _optionsAvailable;
+    }
+    
 }
 
 
@@ -108,17 +106,18 @@ public class WaveFunctionCollapse : MonoBehaviour
     public Vector2Int generateSize = new Vector2Int(2,2);
     public Vector2Int chunckGridSize = new Vector2Int(8, 8);
     public List<Chunck> Chuncks = new List<Chunck>();
-
+    private Options Options;
     
     private GridChunck [] grid ;
     
     // Start is called before the first frame update
     void Start()
     {
+        Options = new Options(Chuncks);
         grid = new GridChunck[generateSize.x * generateSize.y];
         for (int i = 0; i < grid.Length; i++)
         {
-            grid[i] = new GridChunck();
+            grid[i] = new GridChunck(Options.GetAllOptions());
         }
 
         Draw();
@@ -130,14 +129,14 @@ public class WaveFunctionCollapse : MonoBehaviour
    
    
 
-       GridChunck[] gridCopy = grid.OrderBy(gridchunk => { return gridchunk.optionsAvailable.Count; }).ToArray();
-       gridCopy = gridCopy.Where(gridChunck => { return gridChunck.optionsAvailable.Count == gridCopy[0].optionsAvailable.Count; }).ToArray();
+       GridChunck[] gridCopy = grid.OrderBy(gridchunk => { return gridchunk.optionsAvailable.Length ; }).ToArray();
+       gridCopy = gridCopy.Where(gridChunck => { return gridChunck.optionsAvailable.Length == gridCopy[0].optionsAvailable.Length; }).ToArray();
 
        GridChunck chunckChoosed = gridCopy[UnityEngine.Random.Range(0, gridCopy.Length)];
        chunckChoosed.collapsed = true;
-       int optionIndex = UnityEngine.Random.Range(0, chunckChoosed.optionsAvailable.Count);
+       int optionIndex = UnityEngine.Random.Range(0, chunckChoosed.optionsAvailable.Length);
        int option = chunckChoosed.optionsAvailable[optionIndex];
-       chunckChoosed.optionsAvailable = new List<int>() { option };
+       chunckChoosed.optionsAvailable = new int[]{option};
 
        Debug.Log(chunckChoosed);
        
@@ -146,18 +145,11 @@ public class WaveFunctionCollapse : MonoBehaviour
                 GridChunck cell = grid[i + j * generateSize.y];
                 if (cell.collapsed) {
                     int index = cell.optionsAvailable[0];
-                    if (index != 0)
-                    { 
-                        Chunck? chunckFound = Chuncks.FirstOrDefault(c => c.GetChunck(index) != null);
+                   
+                        Chunck? chunckFound = Chuncks.FirstOrDefault(c => c.GetChunck(index, Options) != null);
                         Instantiate(chunckFound.gridToInstanciate,
                             new Vector3(i * chunckGridSize.x, j * chunckGridSize.y, 0), Quaternion.identity);
 
-                        
-                        
-                    }
-                    
-                  
-                    
                 } 
             }
         }
