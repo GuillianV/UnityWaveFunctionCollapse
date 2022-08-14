e6ca0b9cf801fe84f516dc915416dd430bf0e6a2
GuillianV <guillianvibert90@gmail.com>
2022-08-14 21:51:51 +0200

    add scriptable object

--- Assets/Scripts/WaveFunctionCollapse/ChunckData.cs
+++ Assets/Scripts/WaveFunctionCollapse/ChunckData.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "WFC/ChunckData")]
 public class ChunckData : ScriptableObject
 {
-
     public GameObject assetsToInstanciate;
     public SquareEdges Edges;
-
 
 }
